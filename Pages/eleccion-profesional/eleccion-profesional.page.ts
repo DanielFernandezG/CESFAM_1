db2fa5d113e93ce87cf733ac4968ae57259efdde
julio2510 <juliobaezaflores@gmail.com>
2023-10-08 13:38:56 -0300

    profesional BD

--- Pages/eleccion-profesional/eleccion-profesional.page.ts
+++ Pages/eleccion-profesional/eleccion-profesional.page.ts
@@ -1,24 +1,85 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { CitasService } from 'src/app/Services/citas.service';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
 
 @Component({
-  selector: 'app-eleccion-profesional',
+  selector: 'eleccion-profesional',
   templateUrl: 'eleccion-profesional.page.html',
   styleUrls: ['eleccion-profesional.page.scss']
 })
 export class EleccionProfesionalPage {
-  medicos: string[];
+  especialidades: { id: number, nombre: string }[];
+  medicos: { id: number, nombre: string }[];
+  especialidadSeleccionada: number;
+  medicoSeleccionado: { id: number, nombre: string };
 
-  constructor(private citasService: CitasService, private router: Router) {
-    this.medicos = this.citasService.medicosPorEspecialidad[this.citasService.obtenerEspecialidad()] || [];
+  constructor(private sqlite: SQLite) {}
+
+  ionViewWillEnter() {
+    this.cargarEspecialidades();
   }
 
-  seleccionarMedico(medico: string) {
-    this.citasService.guardarMedico(medico);
+  cargarEspecialidades() {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('SELECT ID_Especialidad, Nombre FROM Especialidad', []).then((data) => {
+        this.especialidades = [];
+        if (data.rows.length > 0) {
+          for (let i = 0; i < data.rows.length; i++) {
+            this.especialidades.push({
+              id: data.rows.item(i).ID_Especialidad,
+              nombre: data.rows.item(i).Nombre
+            });
+          }
+        }
+      }).catch((error) => {
+        console.error('Error al cargar especialidades desde la base de datos: ' + JSON.stringify(error));
+      });
+    }).catch((error) => {
+      console.error('Error al abrir la base de datos: ' + JSON.stringify(error));
+    });
+  }
+
+  cargarMedicos(idEspecialidad: number) {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    }).then((db: SQLiteObject) => {
+      db.executeSql('SELECT ID_Doctor, Nombre FROM Doctor WHERE idEspecialidad = ?', [idEspecialidad])
+        .then((data) => {
+          this.medicos = [];
+          if (data.rows.length > 0) {
+            for (let i = 0; i < data.rows.length; i++) {
+              this.medicos.push({
+                id: data.rows.item(i).ID_Doctor,
+                nombre: data.rows.item(i).Nombre
+              });
+            }
+          }
+        })
+        .catch((error) => {
+          console.error('Error al cargar médicos desde la base de datos: ' + JSON.stringify(error));
+        });
+    }).catch((error) => {
+      console.error('Error al abrir la base de datos: ' + JSON.stringify(error));
+    });
+  }
+
+  seleccionarMedico(medico: { id: number, nombre: string }) {
+    this.medicoSeleccionado = medico;
   }
 
   ingresar() {
-    this.router.navigateByUrl('/confirmacion-cita');
+    if (this.especialidadSeleccionada && this.medicoSeleccionado) {
+      // Aquí puedes usar this.especialidadSeleccionada y this.medicoSeleccionado para las operaciones necesarias.
+      console.log('Especialidad seleccionada:', this.especialidadSeleccionada);
+      console.log('Médico seleccionado:', this.medicoSeleccionado);
+      // Continúa con tus operaciones aquí
+    } else {
+      alert('Por favor, seleccione una especialidad y un médico.');
+    }
   }
 }
+
+
