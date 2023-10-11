f41b4d4244f8e6e2f7c4190a9fba0e654bf5b8f9
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-10-10 23:08:11 -0300

    Validacion usuario ya existe

--- Pages/registrar/registrar.page.ts
+++ Pages/registrar/registrar.page.ts
@@ -63,45 +63,73 @@ export class RegistrarPage implements OnInit {
     return rutSinGuion;
   }
 
-  insertData() {
+  async insertData() {
     const run_limpio = this.limpiarRut(this.run)
-    if (validateRut(run_limpio)){
-      if (this.password.length>=8) {
-        if (this.fecha.includes("/")) {
-          if (this.telefono <= 999999999 && this.telefono >= 900000000) {
-            if (this.correo.includes("@") && this.correo.includes(".")) {
-              try{
-                let usu:string='insert into usuario(run,password,tipo,active) values("'+run_limpio+'","'+this.password+'","paciente",0)';
-                this.db.executeSql(usu,[])
-                .then(() => console.log('usuario creado'))
-                .catch(e => alert(JSON.stringify(e)));
+    const usuarioNoExiste = await this.verificarUsuario(run_limpio)
+    if(usuarioNoExiste){
+      if (validateRut(run_limpio)){
+        if (this.password.length>=8) {
+          if (this.fecha.includes("/")) {
+            if (this.telefono <= 999999999 && this.telefono >= 900000000) {
+              if (this.correo.includes("@") && this.correo.includes(".")) {
+                try{
+                  let usu:string='insert into usuario(run,password,tipo,active) values("'+run_limpio+'","'+this.password+'","paciente",0)';
+                  this.db.executeSql(usu,[])
+                  .then(() => {
+                    console.log('usuario creado')}
+                    )
+                  .catch(e => {
+                    console.error(JSON.stringify(e));
+                  });
 
-                let pac:string='insert into paciente (run,nombre,apellido,edad,fechaNacimiento,Direccion,Telefono,Correo,Genero) values("'+run_limpio+'","'+this.nombre+'","'+this.apellidos+'",'+this.edad+',\''+this.fecha+'\', "'+this.direccion+'", "'+this.telefono+'", "'+this.correo+'", "'+this.genero+'")';
-                this.db.executeSql(pac,[])
-                .then(() => console.log('paciente creado'))
-                .catch(e => alert(JSON.stringify(e)));
+                  let pac:string='insert into paciente (run,nombre,apellido,edad,fechaNacimiento,Direccion,Telefono,Correo,Genero) values("'+run_limpio+'","'+this.nombre+'","'+this.apellidos+'",'+this.edad+',\''+this.fecha+'\', "'+this.direccion+'", "'+this.telefono+'", "'+this.correo+'", "'+this.genero+'")';
+                  this.db.executeSql(pac,[])
+                  .then(() => console.log('paciente creado'))
+                  .catch(e => alert(JSON.stringify(e)));
 
-                alert("Usuario creado exitosamente")
-                this.router.navigate(['login']);
-              } catch {
-                alert ("Error al crear usuarios")
+                  alert("Usuario creado exitosamente")
+                  this.router.navigate(['login']);
+                } catch {
+                  alert ("Error al crear usuarios")
+                }
+              } else {
+                alert ("Formato de Correo Incorrecto")
               }
             } else {
-              alert ("Formato de Correo Incorrecto")
+              alert ("Formato de Telefono Incorrecto")
             }
           } else {
-            alert ("Formato de Telefono Incorrecto")
+            alert("Formato de Fecha Incorrecto")
           }
         } else {
-          alert("Formato de Fecha Incorrecto")
+          alert ("La contraseña debe tener al menos 8 caracteres")
         }
       } else {
-        alert ("La contraseña debe tener al menos 8 caracteres")
+        alert ("Run Invalido")
       }
     } else {
-      alert ("Run Invalido")
+      alert ("El Run Indicado ya Posee Cuenta")
     }
   }
+
+  verificarUsuario(run: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.db
+        .executeSql("SELECT * FROM usuario WHERE run=?", [run])
+        .then((result) => {
+          if (result.rows.length === 0) {
+            // El usuario no existe
+            resolve(true);
+          } else {
+            // El usuario existe
+            resolve(false);
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }  
 
   selectData()
   {
