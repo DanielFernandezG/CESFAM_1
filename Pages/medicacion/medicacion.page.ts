44e51ccf799f7811383a35fe27941d0f1aaddb85
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-10-28 21:33:08 -0300

    Actualizacion Notificacion

--- Pages/medicacion/medicacion.page.ts
+++ Pages/medicacion/medicacion.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
-import { LocalNotifications, ScheduleOptions } from "@capacitor/local-notifications";
+import {
+  LocalNotifications,
+  ScheduleOptions,
+} from "@capacitor/local-notifications";
 import { SQLite, SQLiteObject } from "@awesome-cordova-plugins/sqlite/ngx";
 import { Router } from "@angular/router";
 
@@ -16,6 +19,7 @@ export class MedicacionPage implements OnInit {
   notificationInterval: number; // Nuevo campo para el intervalo
   db: SQLiteObject;
   medicamentos: medicamento[];
+  medicamentoData: med[];
 
   constructor(private sqlite: SQLite, private router: Router) {
     this.createOpenDatabase();
@@ -33,6 +37,7 @@ export class MedicacionPage implements OnInit {
         .then((db: SQLiteObject) => {
           this.db = db;
           console.log("Conectado a la base de datos");
+          this.mostrarInfo();
           this.mostrarMedicamentos();
         })
         .catch((e) => console.error("Error al abrir la base de datos:", e));
@@ -41,34 +46,102 @@ export class MedicacionPage implements OnInit {
     }
   }
 
+  mostrarInfo() {
+    this.medicamentoData = [];
+
+    this.db
+      .executeSql("SELECT * FROM usuario WHERE active = 1", [])
+      .then((result) => {
+        if (result.rows.item(0).run != "") {
+          this.db
+            .executeSql("SELECT * FROM Paciente WHERE Run = ?", [
+              result.rows.item(0).run,
+            ])
+            .then((result) => {
+              this.db
+                .executeSql(
+                  "select * from RegistroMedicacion where ID_Paciente=?",
+                  [result.rows.item(0).ID_Paciente]
+                )
+                .then((result) => {
+                  for (let i = 0; i < result.rows.length; i++) {
+                    this.obtenerNombreMedicamento(
+                      result.rows.item(i).ID_Medicamento
+                    )
+                      .then((Medicamento: string) => {
+                        this.medicamentoData.push({
+                          id: result.rows.item(i).ID_RegistroMedicacion,
+                          descripcion: Medicamento,
+                          fechainicio: result.rows.item(i).FechaInicio,
+                          hora: result.rows.item(i).HoraToma,
+                          fechatermino: result.rows.item(i).FechaFin,
+                          intervalo: result.rows.item(i).IntervaloToma,
+                        });
+                      })
+                      .catch((error) => {
+                        console.error("Error al obtener especialidad:", error);
+                      });
+                  }
+                })
+                .catch((e) => alert(JSON.stringify(e)));
+            });
+        } else {
+          this.router.navigate(["login"]);
+        }
+      })
+      .catch((e) => alert(JSON.stringify(e)));
+  }
+
+  eliminarMed(id: string) {
+    this.db.executeSql("DELETE FROM RegistroMedicacion WHERE ID_RegistroMedicacion = ?", [id]);
+    this.mostrarInfo();
+  }
+
+  msotrarAgregar(){
+
+  }
+
+  toggleContent() {
+    this.showContent = !this.showContent;
+  }
+  showContent: boolean = false;
+
   async scheduleNotification() {
-    if (!this.selectedDate || !this.selectedTime || !this.notificationInterval) {
+    if (
+      !this.selectedDate ||
+      !this.selectedTime ||
+      !this.notificationInterval
+    ) {
       alert("Por favor, complete todos los campos.");
       return;
     }
-  
-    const scheduledDateTime = new Date(`${this.selectedDate}T${this.selectedTime}`);
+
+    const scheduledDateTime = new Date(
+      `${this.selectedDate}T${this.selectedTime}`
+    );
     const now = new Date();
     if (scheduledDateTime <= now) {
       alert("La fecha y hora deben ser posteriores a la hora actual.");
       return;
     }
-  
+
     const options: ScheduleOptions = {
-      notifications: []
+      notifications: [],
     };
-  
+
     const numberOfNotifications = Math.floor(24 / this.notificationInterval); // Calcula el número de notificaciones en un día
-  
+
     for (let i = 0; i < numberOfNotifications; i++) {
       const currentDateTime = new Date(scheduledDateTime);
-      currentDateTime.setHours(scheduledDateTime.getHours() + i * this.notificationInterval);
-  
+      currentDateTime.setHours(
+        scheduledDateTime.getHours() + i * this.notificationInterval
+      );
+
       if (currentDateTime <= now) {
         // No progresa notificaciones pasadas
         continue;
       }
-  
+
       options.notifications.push({
         id: i + 1,
         title: "Recordatorio de medicación",
@@ -76,26 +149,27 @@ export class MedicacionPage implements OnInit {
         schedule: { at: currentDateTime },
       });
     }
-  
+
     if (options.notifications.length === 0) {
       alert("No hay notificaciones futuras programadas.");
       return;
     }
-  
+
     try {
       await LocalNotifications.schedule(options);
       alert("Notificaciones programadas con éxito.");
-  
+
       // Formatea la fecha de finalización en el formato deseado (YYYY-MM-DD)
       const formattedEndDate = this.formatDate(new Date(this.selectedEndDate));
-  
+
       // Almacena los datos en la tabla de la base de datos
       this.insertMedicationData(formattedEndDate);
+      this.toggleContent();
     } catch (ex) {
       alert("Error al programar las notificaciones: " + JSON.stringify(ex));
     }
   }
-  
+
   formatDate(date: Date): string {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
@@ -121,18 +195,33 @@ export class MedicacionPage implements OnInit {
                   FechaInicio: this.selectedDate,
                   FechaFin: formattedEndDate, // Usamos la fecha formateada
                   HoraToma: this.selectedTime,
-                  IntervaloToma: this.notificationInterval,
-                  EstadoToma: "Pendiente",
+                  IntervaloToma: this.notificationInterval
                 };
-  
+
                 // Inserta los datos en la tabla de la base de datos
                 this.db
-                  .executeSql("INSERT INTO RegistroMedicacion (ID_Paciente, ID_Medicamento, FechaInicio, FechaFin, HoraToma, IntervaloToma, EstadoToma) VALUES (?, ?, ?, ?, ?, ?, ?)",[data.ID_Paciente,data.ID_Medicamento,data.FechaInicio,data.FechaFin,data.HoraToma,data.IntervaloToma,data.EstadoToma])
+                  .executeSql(
+                    "INSERT INTO RegistroMedicacion (ID_Paciente, ID_Medicamento, FechaInicio, FechaFin, HoraToma, IntervaloToma) VALUES (?, ?, ?, ?, ?, ?)",
+                    [
+                      data.ID_Paciente,
+                      data.ID_Medicamento,
+                      data.FechaInicio,
+                      data.FechaFin,
+                      data.HoraToma,
+                      data.IntervaloToma
+                    ]
+                  )
                   .then(() => {
-                    console.log("Datos de medicación insertados en la base de datos.");
+                    console.log(
+                      "Datos de medicación insertados en la base de datos."
+                    );
+                    this.mostrarInfo();
                   })
                   .catch((error) => {
-                    console.error("Error al insertar datos de medicación: ",error);
+                    console.error(
+                      "Error al insertar datos de medicación: ",
+                      error
+                    );
                   });
               });
           } else {
@@ -146,24 +235,43 @@ export class MedicacionPage implements OnInit {
   mostrarMedicamentos() {
     this.medicamentos = [];
 
-    this.db
-      .executeSql("select * from Medicamento ", [])
-      .then((result) => {
-        for (let i = 0; i < result.rows.length; i++) {
-          this.medicamentos.push({
-            id: result.rows.item(i).ID_Medicamento,
-            descripcion: result.rows.item(i).NombreMedicamento,
-          });
+    this.db.executeSql("select * from Medicamento ", []).then((result) => {
+      for (let i = 0; i < result.rows.length; i++) {
+        this.medicamentos.push({
+          id: result.rows.item(i).ID_Medicamento,
+          descripcion: result.rows.item(i).NombreMedicamento,
+        });
+      }
+    });
+  }
+
+  async obtenerNombreMedicamento(idMedicamento: string): Promise<string> {
+    return new Promise<string>(async (resolve, reject) => {
+      try {
+        const result = await this.db.executeSql(
+          "SELECT NombreMedicamento FROM Medicamento WHERE ID_Medicamento=?",
+          [idMedicamento]
+        );
+
+        if (result.rows.length > 0) {
+          const nombreMedicamento = result.rows.item(0).NombreMedicamento;
+          resolve(nombreMedicamento);
+        } else {
+          reject(new Error("Medicamento no encontrado"));
         }
-      });
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   async obtenerIdMedicamento(nombreMedicamento: string): Promise<string> {
     return new Promise<string>((resolve, reject) => {
       this.db
-        .executeSql("SELECT ID_Medicamento FROM Medicamento WHERE NombreMedicamento=?", [
-          nombreMedicamento,
-        ])
+        .executeSql(
+          "SELECT ID_Medicamento FROM Medicamento WHERE NombreMedicamento=?",
+          [nombreMedicamento]
+        )
         .then((result) => {
           if (result.rows.length > 0) {
             const idMedicamento = result.rows.item(0).ID_Medicamento;
@@ -182,4 +290,13 @@ export class MedicacionPage implements OnInit {
 class medicamento {
   public id: string;
   public descripcion: string;
+}
+
+class med {
+  public id: string;
+  public descripcion: string;
+  public fechainicio: string;
+  public hora: string;
+  public fechatermino: string;
+  public intervalo: string;
 }
