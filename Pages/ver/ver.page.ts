fe38f199d7ff9bce4db25cd1cb7a50cf477a9674
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-11-06 15:56:41 -0300

    Actualizacion Visualizacion documentos

--- Pages/ver/ver.page.ts
+++ Pages/ver/ver.page.ts
@@ -15,6 +15,7 @@ export class VerPage {
   documentVisible: boolean[] = []; // Arreglo para rastrear la visibilidad de los documentos
   nombresDocumentos: string[] = []; // Arreglo para almacenar los nombres de los documentos
   fechasCreacion: string[] = []; // Arreglo para almacenar las fechas de creación
+  imageBase64: string | null; 
 
   constructor(
     private sqlite: SQLite,
@@ -103,5 +104,74 @@ export class VerPage {
   setClose(isOpen: boolean) {
     this.isModalOpen = isOpen;
     this.documentVisible[this.id] = !this.documentVisible[this.id];
+  }
+
+  toggleContent() {
+    this.showContent = !this.showContent;
+  }
+  showContent: boolean = false;
+
+  onImageSelected(event: any) { 
+    const file = event?.target?.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imageBase64 = e?.target?.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  uploadImage() { 
+    if (this.imageBase64) {
+      this.saveImageToDatabase(this.imageBase64);
+      this.imageBase64 = null;
+      this.fetchAndDisplayDocuments();
+      this.toggleContent();
+    } else {
+      console.log('Ninguna imagen seleccionada para subir.');
+    }
+  }
+
+  saveImageToDatabase(imageContent: string) {
+    // Primero, obtén el ID del paciente activo
+    this.db
+      .executeSql("SELECT ID_Paciente FROM paciente WHERE run = (SELECT run FROM usuario WHERE active = 1)", [])
+      .then((result) => {
+        if (result.rows.length > 0) {
+          const pacienteID = result.rows.item(0).ID_Paciente;
+  
+          // Define los datos de la imagen
+          const currentDate = new Date();
+          const formattedDate = currentDate.toISOString().slice(0, 10); // Formato "YYYY-MM-DD"
+          
+          const data = {
+            ID_Paciente: pacienteID,
+            TipoDocumento: 'Imagen',
+            NombreDocumento: 'Imagen.png', // Cambia el nombre del archivo de imagen
+            ContenidoDocumento: imageContent, // Guarda la imagen en formato Base64 como una cadena
+            FechaCreacion: formattedDate, // Fecha en formato "YYYY-MM-DD"
+          };
+  
+          // Inserta la imagen en la tabla DocumentoMedico
+          this.db
+            .executeSql(
+              'INSERT INTO DocumentoMedico (ID_Paciente, TipoDocumento, NombreDocumento, ContenidoDocumento, FechaCreacion) VALUES (?, ?, ?, ?, ?)',
+              [data.ID_Paciente, data.TipoDocumento, data.NombreDocumento, data.ContenidoDocumento, data.FechaCreacion]
+            )
+            .then(() => {
+              console.log('Imagen guardada en la base de datos.');
+              alert("Imagen Médica Subida");
+            })
+            .catch((error) => {
+              console.error('Error al insertar la imagen en la base de datos:', error);
+            });
+        } else {
+          console.error('No se encontró un paciente activo en la base de datos.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error al obtener el ID del paciente activo:', error);
+      });
   }
 }
