fc1eb5d69dc88d6b156dce98bbad2e0505c64617
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-11-06 15:38:57 -0300

    Revert "Revert "Actualizacion visor pdf""
    This reverts commit 8444fcb9e1669228a61e8990237d1b40094d73c0.

--- Pages/ver/ver.page.ts
+++ Pages/ver/ver.page.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Component } from "@angular/core";
+import { SQLite, SQLiteObject } from "@awesome-cordova-plugins/sqlite/ngx";
+import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-ver',
-  templateUrl: './ver.page.html',
-  styleUrls: ['./ver.page.scss'],
+  selector: "app-ver",
+  templateUrl: "./ver.page.html",
+  styleUrls: ["./ver.page.scss"],
 })
 export class VerPage {
   db: SQLiteObject;
@@ -14,49 +15,91 @@ export class VerPage {
   nombresDocumentos: string[] = []; // Arreglo para almacenar los nombres de los documentos
   fechasCreacion: string[] = []; // Arreglo para almacenar las fechas de creación
 
-  constructor(private sqlite: SQLite, private sanitizer: DomSanitizer) {
+  constructor(
+    private sqlite: SQLite,
+    private sanitizer: DomSanitizer,
+    private router: Router
+  ) {
     this.createOpenDatabase();
   }
 
   createOpenDatabase() {
     this.sqlite
       .create({
-        name: 'data.db',
-        location: 'default',
+        name: "data.db",
+        location: "default",
       })
       .then((db: SQLiteObject) => {
         this.db = db;
-        console.log('Conectado a la base de datos');
+        console.log("Conectado a la base de datos");
         this.fetchAndDisplayDocuments(); // Llama a la función para cargar los documentos
       })
-      .catch((e) => console.error('Error al abrir la base de datos:', e));
+      .catch((e) => console.error("Error al abrir la base de datos:", e));
   }
 
   fetchAndDisplayDocuments() {
-    this.db.executeSql('SELECT ContenidoDocumento, NombreDocumento, FechaCreacion FROM DocumentoMedico', []).then((result) => {
-      const documents = [];
-      const nombresDocumentos = [];
-      const fechasCreacion = [];
+    this.db
+      .executeSql("select * from usuario where active = 1", [])
+      .then((result) => {
+        if (result.rows.item(0).run != "") {
+          this.db
+            .executeSql("select * from paciente where run=?", [
+              result.rows.item(0).run,
+            ])
+            .then((result) => {
+              this.db
+                .executeSql(
+                  "SELECT ContenidoDocumento, NombreDocumento, FechaCreacion FROM DocumentoMedico WHERE ID_Paciente=?",
+                  [result.rows.item(0).ID_Paciente]
+                )
+                .then((result) => {
+                  const documents = [];
+                  const nombresDocumentos = [];
+                  const fechasCreacion = [];
 
-      for (let i = 0; i < result.rows.length; i++) {
-        const documentBase64 = result.rows.item(i).ContenidoDocumento;
-        documents.push(this.sanitizer.bypassSecurityTrustResourceUrl(documentBase64) as string);
-        nombresDocumentos.push(result.rows.item(i).NombreDocumento);
-        fechasCreacion.push(result.rows.item(i).FechaCreacion);
-      }
+                  for (let i = 0; i < result.rows.length; i++) {
+                    const documentBase64 =
+                      result.rows.item(i).ContenidoDocumento;
+                    documents.push(
+                      this.sanitizer.bypassSecurityTrustResourceUrl(
+                        documentBase64
+                      ) as string
+                    );
+                    nombresDocumentos.push(result.rows.item(i).NombreDocumento);
+                    fechasCreacion.push(result.rows.item(i).FechaCreacion);
+                  }
 
-      // Inicializa las matrices de visibilidad y almacena la lista de documentos junto con los nombres y fechas.
-      this.documentVisible = new Array(documents.length).fill(false);
-      this.documents = documents;
-      this.nombresDocumentos = nombresDocumentos;
-      this.fechasCreacion = fechasCreacion;
-    }).catch((error) => {
-      console.error('Error al obtener los documentos desde la base de datos:', error);
-    });
+                  // Inicializa las matrices de visibilidad y almacena la lista de documentos junto con los nombres y fechas.
+                  this.documentVisible = new Array(documents.length).fill(
+                    false
+                  );
+                  this.documents = documents;
+                  this.nombresDocumentos = nombresDocumentos;
+                  this.fechasCreacion = fechasCreacion;
+                })
+                .catch((error) => {
+                  console.error(
+                    "Error al obtener los documentos desde la base de datos:",
+                    error
+                  );
+                });
+            });
+        } else {
+          this.router.navigate(["login"]);
+        }
+      })
+      .catch((e) => alert(JSON.stringify(e)));
   }
 
-  toggleDocumento(index: number) {
-    // Cambia el estado de visibilidad del documento en el índice especificado.
+  // toggleDocumento(index: number) {
+  //   // Cambia el estado de visibilidad del documento en el índice especificado.
+  //   this.documentVisible[index] = !this.documentVisible[index];
+  // }
+
+  isModalOpen = false;
+
+  setOpen(isOpen: boolean, index: number) {
     this.documentVisible[index] = !this.documentVisible[index];
+    this.isModalOpen = isOpen;
   }
 }
