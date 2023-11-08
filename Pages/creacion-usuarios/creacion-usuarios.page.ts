2804a04905888eca4e63196ed3ddf44fdfa78e2e
Tato1272 <81763862+Tato1272@users.noreply.github.com>
2023-11-07 22:50:06 -0300

    Creacion de usuario admin

--- Pages/creacion-usuarios/creacion-usuarios.page.ts
+++ Pages/creacion-usuarios/creacion-usuarios.page.ts
@@ -15,20 +15,25 @@ export class CreacionUsuariosPage implements OnInit {
   confPassword: string;
   nombre: string;
   apellidos: string;
-  edad: number;
   fecha: string;
-  direccion: string;
-  telefono: number;
   correo: string;
   genero: string;
+  rol: string;
+  esp: string;
+  cesfam: string;
+  estudios: string;
   usuarioData: usuario[];
+  especialidades: especialidad[];
+  isDoctor: boolean = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private sqlite:SQLite) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.createOpenDatabase();
+  }
 
   async insertData() {
-    if(this.run=="" || this.password=="" || this.nombre=="" || this.apellidos=="" || this.edad==null || this.fecha=="" || this.direccion=="" || this.telefono==null || this.correo=="" || this.genero==""){
+    if(this.rol=="" || this.run=="" || this.password=="" || this.nombre=="" || this.apellidos=="" || this.fecha=="" || this.correo=="" || this.genero==""){
       alert("Debe Llenar Todos los Campos")
     } else {
       const run_limpio = this.limpiarRut(this.run)
@@ -38,33 +43,66 @@ export class CreacionUsuariosPage implements OnInit {
           if (this.password.length>=8) {
             if(this.password==this.confPassword){
               if (this.fecha.includes("/")) {
-                if (this.telefono <= 999999999 && this.telefono >= 900000000) {
-                  if (this.correo.includes("@") && this.correo.includes(".")) {
-                    try{
-                      let usu:string='insert into usuario(run,password,tipo,active) values("'+run_limpio+'","'+this.password+'","paciente",0)';
-                      this.db.executeSql(usu,[])
-                      .then(() => {
-                        console.log('usuario creado')}
-                        )
-                      .catch(e => {
-                        console.error(JSON.stringify(e));
-                      });
+                if (this.correo.includes("@") && this.correo.includes(".")) {
+                  try{
+                    
+                    let usu:string='insert into usuario(run,password,tipo,active) values("'+run_limpio+'","'+this.password+'","'+this.rol+'",0)';
+                    this.db.executeSql(usu,[])
+                    .then(() => {
+                      console.log('usuario creado')}
+                      )
+                    .catch(e => {
+                      console.error(JSON.stringify(e));
+                    });
 
-                      let pac:string='insert into paciente (run,nombre,apellido,edad,fechaNacimiento,Direccion,Telefono,Correo,Genero) values("'+run_limpio+'","'+this.nombre+'","'+this.apellidos+'",'+this.edad+',\''+this.fecha+'\', "'+this.direccion+'", '+this.telefono+', "'+this.correo+'", "'+this.genero+'")';
-                      this.db.executeSql(pac,[])
-                      .then(() => console.log('paciente creado'))
+                    if(this.rol == "doctor"){
+                      this.obtenerIdEspecialidad(this.esp).then((idesp: number) => {
+                      let doc:string='insert into Doctor (Run, Nombre, Apellido, ID_Especialidad, CESFAM, Estudios, FechaNacimiento, Genero, Correo) values("'+run_limpio+'","'+this.nombre+'","'+this.apellidos+'", '+idesp+', "'+this.cesfam+'","'+this.estudios+'",\''+this.fecha+'\', "'+this.genero+'","'+this.correo+'")';
+                      this.db.executeSql(doc,[])
+                      .then(() => console.log('usuario creado'))
                       .catch(e => alert(JSON.stringify(e)));
 
-                      alert("Usuario creado exitosamente")
-                      this.router.navigate(['login']);
-                    } catch {
-                      alert ("Error al crear usuarios")
+                      alert("Doctor creado exitosamente")
+                      this.run = "";
+                      this.password = "";
+                      this.confPassword = "";
+                      this.nombre = "";
+                      this.apellidos = "";
+                      this.fecha = "";
+                      this.correo = "";
+                      this.genero = "";
+                      this.rol = "";
+                      this.esp = "";
+                      this.cesfam = "";
+                      this.estudios = "";
+                    })
+                    .catch((error) => {
+                      console.error("Error al obtener especialidad:", error);
+                    });
+                    } else if(this.rol == "secretaria") {
+                      let sec:string='insert into Secretaria (Run, Nombre, Apellido, FechaNacimiento, Correo, Genero) values("'+run_limpio+'","'+this.nombre+'","'+this.apellidos+'",\''+this.fecha+'\',"'+this.correo+'", "'+this.genero+'")';
+                      this.db.executeSql(sec,[])
+                      .then(() => console.log('usuario creado'))
+                      .catch(e => alert(JSON.stringify(e)));
+
+                      alert("Secretaria creada exitosamente")
+                      this.run = "";
+                      this.password = "";
+                      this.confPassword = "";
+                      this.nombre = "";
+                      this.apellidos = "";
+                      this.fecha = "";
+                      this.correo = "";
+                      this.genero = "";
+                      this.rol = "";
+                    } else {
+                      alert("Ingrese Rol")
                     }
-                  } else {
-                    alert ("Formato de Correo Incorrecto")
+                  } catch {
+                    alert ("Error al agregar usuarios")
                   }
                 } else {
-                  alert ("Formato de Telefono Incorrecto")
+                  alert ("Formato de Correo Incorrecto")
                 }
               } else {
                 alert("Formato de Fecha Incorrecto")
@@ -82,6 +120,10 @@ export class CreacionUsuariosPage implements OnInit {
         alert ("El Run Indicado ya Posee Cuenta")
       }
     }
+  }
+
+  onRolChange() {
+    this.isDoctor = this.rol === 'doctor';
   }
 
   limpiarRut(rut: string): string {
@@ -112,9 +154,61 @@ export class CreacionUsuariosPage implements OnInit {
         });
     });
   }  
+
+  mostrarEspecialidades() {
+    this.especialidades = [];
+
+    this.db.executeSql("select * from especialidad", []).then((result) => {
+      for (let i = 0; i < result.rows.length; i++) {
+        this.especialidades.push({
+          id: result.rows.item(i).ID_Especialidad,
+          descripcion: result.rows.item(i).Nombre,
+        });
+      }
+    });
+  }
+
+  async obtenerIdEspecialidad(nombreEspecialidad: string): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      this.db
+        .executeSql("SELECT ID_Especialidad FROM especialidad WHERE Nombre=?", [
+          nombreEspecialidad,
+        ])
+        .then((result) => {
+          if (result.rows.length > 0) {
+            const idEspecialidad = result.rows.item(0).ID_Especialidad;
+            resolve(idEspecialidad);
+          } else {
+            reject(new Error("Especialidad no encontrada"));
+          }
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  createOpenDatabase() {
+    this.sqlite
+      .create({
+        name: "data.db",
+        location: "default",
+      })
+      .then((db: SQLiteObject) => {
+        this.db = db;
+        console.log("Conectado a la base de datos");
+        this.mostrarEspecialidades();
+      })
+      .catch((e) => console.log("Error al conectar a la base de datos: ", e));
+  }
 }
 
 class usuario{
   public run:string;
   public password:string;
+}
+
+class especialidad {
+  public id: string;
+  public descripcion: string;
 }
